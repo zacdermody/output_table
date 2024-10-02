937a99b411787fce736252d5fdcf19f129918673
zacdermody <zacdermody@gmail.com>
2024-10-01 21:38:30 -0400

    reversal

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/output_table/',  // Replace 'your-repo-name' with the actual GitHub repo name
   plugins: [react()],
-})
+}
+)
